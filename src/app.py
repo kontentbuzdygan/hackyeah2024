ba57f5459019b2c9498afaa9fd3f5bcb3051b7f4
Szustakowski, Mikolaj <mikolaj.szustakowski@intel.com>
2024-09-28 13:20:34 +0200

    submit button

--- src/app.py
+++ src/app.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.write("BreakWordTraps")
 
-videos = st.file_uploader("Drop the video you want to classify!", accept_multiple_files=True)
+with st.form(key="upload"):
+    videos = st.file_uploader(label="Drop the video you want to classify!", accept_multiple_files=True)
+    submit = st.form_submit_button(label="Classify")
