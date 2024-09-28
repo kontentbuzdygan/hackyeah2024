4abd01feea8f265ce99db51ad68ed675e05de096
michalwa <michalwa2003@gmail.com>
2024-09-28 15:51:18 +0200

    feat: audio extraction

--- src/app.py
+++ src/app.py
@@ -1,10 +1,20 @@
 import streamlit as st
+from moviepy.editor import VideoFileClip
+from tempfile import NamedTemporaryFile
 
 st.write("BreakWordTraps")
 
-videos = st.file_uploader(label="Drop the video you want to classify!", accept_multiple_files=True)
-st.button(label="Classify")
+video = st.file_uploader(label="Drop the video you want to classify!")
 
-if videos:
-    for video in videos:
-        st.video(video)
+if video:
+    video_tmp = NamedTemporaryFile()
+    video_tmp.write(video.getbuffer())
+
+    st.video(video_tmp.name)
+
+if st.button(label="Classify!", type="primary"):
+    clip = VideoFileClip(video_tmp.name)
+    audio_tmp = NamedTemporaryFile(suffix=".mp3")
+    clip.audio.write_audiofile(audio_tmp.name)
+
+    st.audio(audio_tmp.name)
