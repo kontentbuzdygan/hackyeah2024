22c936bf8aaea85d872021be034cf227091c3931
Szustakowski, Mikolaj <mikolaj.szustakowski@intel.com>
2024-09-28 13:30:06 +0200

    video preview

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.write("BreakWordTraps")
 
-with st.form(key="upload"):
-    videos = st.file_uploader(label="Drop the video you want to classify!", accept_multiple_files=True)
-    submit = st.form_submit_button(label="Classify")
+videos = st.file_uploader(label="Drop the video you want to classify!", accept_multiple_files=True)
+st.button(label="Classify")
+
+if videos:
+    for video in videos:
+        st.video(video)
